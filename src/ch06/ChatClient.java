4a1af6414c2c444e252280bfb45fa5486e928617
kyeonghooon <gibbon0510@gmail.com>
2024-06-03 17:30:03 +0900

    chat

--- src/ch06/ChatClient.java
+++ src/ch06/ChatClient.java
@@ -11,7 +11,7 @@ public class ChatClient extends AbstractClient {
 	@Override
 	protected void connectToServer() throws IOException {
 		// AbstractClient --> 부모 클래스 --> 서버측과 연결된 소켓을 주입해주어야 한다.
-		super.setSocket(new Socket("192.168.0.48", 5000));
+		super.setSocket(new Socket("192.168.0.132", 5000));
 		
 	}
 	
